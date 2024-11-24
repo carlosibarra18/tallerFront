4e5d5ebc15fe15a9e3c75a3ba5e9505ab01fb327
carlos <cibarra4@cuc.edu.co>
2024-11-23 22:38:56 -0500

    ajustes3

--- src/app/componentes/otros/Acerca.tsx
+++ src/app/componentes/otros/Acerca.tsx
@@ -1,6 +1,7 @@
 import foto01 from "../../../assets/img/listarLibros.jpg"
 import foto02 from "../../../assets/img/images.jpeg"
 import foto03 from "../../../assets/img/images2.jpeg"
+import { NavLink } from "react-router-dom"
 export const Acerca = () => {
     return (
         <>
@@ -10,7 +11,7 @@ export const Acerca = () => {
                     <div className="card-body">
                         <h5 className="card-title">Listar Libros</h5>
                         <p className="card-text">Aqui puedes encontrar nuestro catalogo de libros.</p>
-                        <a href="/liblis" className="btn btn-primary">Ir a listar libros</a>
+                        <NavLink to="/liblis" className="btn btn-primary">Ir a listar libros</NavLink>
                     </div>
                 </div>
                 <div className="card" style={{width: "18rem"}}>
@@ -18,7 +19,7 @@ export const Acerca = () => {
                     <div className="card-body">
                         <h5 className="card-title">Registrar libros</h5>
                         <p className="card-text">Aqui puedes registrar tus libros.</p>
-                        <a href="/libcre" className="btn btn-primary">Ir a administrar libros</a>
+                        <NavLink to="/libcre" className="btn btn-primary">Ir a administrar libros</NavLink>
                     </div>
                 </div>
                 <div className="card" style={{width: "18rem"}}>
@@ -26,7 +27,7 @@ export const Acerca = () => {
                     <div className="card-body">
                         <h5 className="card-title">Admin libros</h5>
                         <p className="card-text">Aqui puedes administrar tus libros.</p>
-                        <a href="/libadm" className="btn btn-primary">Ir a administrar libros</a>
+                        <NavLink to="/libadm" className="btn btn-primary">Ir a administrar libros</NavLink>
                     </div>
                 </div>
             </div>
