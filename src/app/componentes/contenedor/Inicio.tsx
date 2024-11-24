a2202a071ff4c90316c8585089a10bcf4fbda841
carlos <cibarra4@cuc.edu.co>
2024-11-23 22:09:30 -0500

    nuevos ajustes

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,9 +1,45 @@
+import image1  from "../../../assets/img/fotoLibro1.jpeg"
+import image2 from "../../../assets/img/fotoLibro2.jpg"
+import image3 from "../../../assets/img/fotoLibro3.jpeg"
 export const Inicio = () => {
   return (
     <>
-      <br /><br />
-      Soy el Inicio <br />
-      Como el de la bienvenida...
+      <div id="carouselExampleFade" className="carousel slide carousel-fade">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img 
+            src={image1} 
+            className="d-block w-100" 
+            alt="..."
+            width={1896}
+            height={550}/>
+          </div>
+          <div className="carousel-item">
+            <img 
+            src={image2} 
+            className="d-block w-100" 
+            alt="..."
+            width={1896}
+            height={550}/>
+          </div>
+          <div className="carousel-item">
+            <img 
+            src={image3} 
+            className="d-block w-100" 
+            alt="..."
+            width={1896}
+            height={550}/>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </>
   );
 };
--- src/app/componentes/otros/Acerca.tsx
+++ src/app/componentes/otros/Acerca.tsx
@@ -1,8 +1,35 @@
+import foto01 from "../../../assets/img/listarLibros.jpg"
+import foto02 from "../../../assets/img/images.jpeg"
+import foto03 from "../../../assets/img/images2.jpeg"
 export const Acerca = () => {
     return (
         <>
-        Soy el Acerca De <br />
-        El de la informacion...
+            <div className="d-flex justify-content-around flex-wrap">
+                <div className="card" style={{width: "18rem"}}>
+                    <img src={foto01} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Listar Libros</h5>
+                        <p className="card-text">Aqui puedes encontrar nuestro catalogo de libros.</p>
+                        <a href="/liblis" className="btn btn-primary">Ir a listar libros</a>
+                    </div>
+                </div>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src={foto02} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Registrar libros</h5>
+                        <p className="card-text">Aqui puedes crear tus libros.</p>
+                        <a href="/libadm" className="btn btn-primary">Ir a crear libros</a>
+                    </div>
+                </div>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src={foto03} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Admin libros</h5>
+                        <p className="card-text">Aqui puedes administrar tus libros.</p>
+                        <a href="/libadm" className="btn btn-primary">Ir a administrar libros</a>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
